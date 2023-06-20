e025a1f304de6eb10f686581b32a35d064a105ee
albert585 <nnnn565@163.com>
2023-06-20 22:24:24 +0800

    update

--- detect.py
+++
@@ -1,7 +0,0 @@
-import os
-import wget
-def detect():
-    if os.path.exists("/usr/bin/pacman"):
-        print("检测到您的系统为ArchLinux或其他Arch系系统")
-        wget.download("https://www.linuxfromscratch.org/blfs/downloads/stable/BLFS-BOOK-11.3-nochunks.html")
-detect()
